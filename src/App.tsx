518980d9b68a02caa8ac3b1910a9cf5cb7fba4d1
Lachicagladiadora <200379@unsaac.edu.pe>
2023-12-19 23:43:39 -0500

    build: create repository and make template in jsx

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+function App() {
+  return (
+    <div>
+      <header>
+        <h1>pikachu</h1>
+      </header>
+      <main>
+        <section>tipo</section>
+        <section>ataques</section>
+      </main>
+    </div>
+  );
+}
+
+export default App;
