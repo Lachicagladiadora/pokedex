fb551baf598819b0721b7ec850b636f6e15cf958
Lachicagladiadora <200379@unsaac.edu.pe>
2023-12-19 23:56:07 -0500

    wip: template in jsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,28 @@ function App() {
     <div>
       <header>
         <h1>pikachu</h1>
+        <p>25</p>
       </header>
       <main>
-        <section>tipo</section>
-        <section>ataques</section>
+        <button>shiny</button>
+        <section>
+          <img src="" alt="pikachu" />
+        </section>
+        <section>
+          description
+          <ul>
+            <li>type</li>
+            <li>heigth</li>
+            <li>weigth</li>
+          </ul>
+        </section>
+        <section>
+          ataques/moves
+          <ul>
+            <li>electrico</li>
+          </ul>
+        </section>
+        <section>location area encounter</section>
       </main>
     </div>
   );
